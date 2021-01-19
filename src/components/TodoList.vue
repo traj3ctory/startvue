8d5e796a86890a604ed0e42241e3fad7ddf46acb
traj3ctory <davidbenard.bd@gmail.com>
2021-01-19 14:42:03 +0100

    almost_complete

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,46 +1,67 @@
 <template>
   <div>
-    <p class="tasks">Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
-    <p class="tasks">Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    
+    <p class="tasks">
+      Completed Tasks:
+      {{
+        todos.filter((todo) => {
+          return todo.done === true;
+        }).length
+      }}
+    </p>
+    <p class="tasks">
+      Pending Tasks:
+      {{
+        todos.filter((todo) => {
+          return todo.done === false;
+        }).length
+      }}
+    </p>
+
     <div class="container">
       <div class="row">
-        <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :todo.sync="todo" :key="todo.id"></todo>
+        <todo
+          v-on:delete-todo="deleteTodo"
+          v-on:complete-todo="completeTodo"
+          v-for="todo in todos"
+          :todo.sync="todo"
+          :key="todo.id"
+        ></todo>
       </div>
     </div>
   </div>
 </template>
 
-<script type = "text/javascript" >
-import sweetalert from 'sweetalert';
-import Todo from './Todo';
+<script type="text/javascript">
+import sweetalert from "sweetalert";
+import Todo from "./Todo";
 
 export default {
-  props: ['todos'],
+  props: ["todos"],
   components: {
     Todo,
   },
   methods: {
     deleteTodo(todo) {
-      sweetalert({
-        title: 'Are you sure?',
-        text: 'This To-Do will be permanently deleted!',
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#DD6B55',
-        confirmButtonText: 'Yes, delete it!',
-        closeOnConfirm: false,
-      },
-      () => {
-        const todoIndex = this.todos.indexOf(todo);
-        this.todos.splice(todoIndex, 1);
-        sweetalert('Deleted!', 'Your To-Do has been deleted.', 'success');
-      });
+      // sweetalert(
+      //   {
+      //   title: 'Are you sure?',
+      //   text: 'This To-Do will be permanently deleted!',
+      //   type: 'warning',
+      //   showCancelButton: true,
+      //   confirmButtonColor: '#DD6B55',
+      //   confirmButtonText: 'Yes, delete it!',
+      //   closeOnConfirm: false,
+      // },
+      // () => {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+      sweetalert("Deleted!", "Your To-Do has been deleted.", "success");
+      // });
     },
     completeTodo(todo) {
       const todoIndex = this.todos.indexOf(todo);
       this.todos[todoIndex].done = true;
-      sweetalert('Success!', 'To-Do completed!', 'success');
+      sweetalert("Success!", "To-Do completed!", "success");
     },
   },
 };
@@ -51,4 +72,3 @@ p.tasks {
   text-align: center;
 }
 </style>
-
